90516fb2da8e9aba57daf0a2be3787ae6021e7c2
John <john@un-net.com>
2018-07-15 23:18:33 +0800

    mdbs

--- src/main/java/com/mdbs/interceptor/CommonInterceptor.java
+++ src/main/java/com/mdbs/interceptor/CommonInterceptor.java
@@ -16,28 +16,7 @@ public class CommonInterceptor implements HandlerInterceptor {
     public void setExcludedUrls(List<String> excludedUrls) {
         this.excludedUrls = excludedUrls;
     }*/
-    /**
-     * 
-     * @Description: 
-     * 在业务处理器处理请求之前被调用 如果返回false 
-     * 从当前的拦截器往回执行所有拦截器的afterCompletion(),
-     * 再退出拦截器链, 如果返回true 执行下一个拦截器,
-     * 直到所有的拦截器都执行完毕 再执行被拦截的Controller
-     * 然后进入拦截器链,
-     * 从最后一个拦截器往回执行所有的postHandle()
-     * 接着再从最后一个拦截器往回执行所有的afterCompletion()
-     * 
-     * @param  request
-     * 
-     * @param  response
-     * 
-     * @return: boolean
-     * 
-     * @author: SongJia
-     * 
-     * @date: 2016-6-27 下午4:17:51
-     * 
-     */
+    
     public boolean preHandle(HttpServletRequest request,
             HttpServletResponse response, Object handler) throws Exception {
     	response.setHeader("Access-Control-Allow-Origin", "*");
@@ -57,26 +36,7 @@ public class CommonInterceptor implements HandlerInterceptor {
 
     }
 
-    /**
-     * 
-     * @Description:
-     * 在DispatcherServlet完全处理完请求后被调用
-     * 当有拦截器抛出异常时,
-     * 会从当前拦截器往回执行所有的拦截器的afterCompletion()
-     * 
-     * @param request
-     * 
-     * @param response
-     * 
-     * @param handler
-     * 
-     * @param ex
-     * 
-     * @author: SongJia
-     * 
-     * @date: 2016-6-27 下午4:27:51
-     * 
-     */
+    
     public void afterCompletion(HttpServletRequest request,
             HttpServletResponse response, Object handler, Exception ex)
             throws Exception {
